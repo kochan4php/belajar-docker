10d69bf190ff6053b1fa2c96743622ed60c646a5
kochan4php <aprodeosubarno@gmail.com>
2023-06-28 17:21:16 +0700

    belajar docker image

---
+++ belajar-docker-dasar/docker-image.sh
@@ -0,0 +1,8 @@
+# Melihat semua docker image yang tersedia di docker daemon
+sudo docker image ls
+
+# Mendownload docker image dari docker registry
+sudo docker image pull namaimage:tag
+
+# Menghapus docker image dari docker daemon
+sudo docker image rm namaimage:tag

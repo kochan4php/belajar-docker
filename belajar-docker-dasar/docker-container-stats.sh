8b95f183bba49a29a406fa5f340d32f6e160227a
kochan4php <aprodeosubarno@gmail.com>
2023-07-01 02:58:48 +0700

    belajar melihat statistik docker container

---
+++ belajar-docker-dasar/docker-container-stats.sh
@@ -0,0 +1,3 @@
+# Docker memiliki kemampuan untuk melihat jumlah penggunaan resource cpu dan memory dari tiap-tiap container yang sedang berjalan.
+# Untuk melihat penggunaan resource dari tiap-tiap container, gunakan perintah docker container stats.
+sudo docker container stats

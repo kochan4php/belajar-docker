a4e6def16bd0d9d287779cf96267e81107be592f
kochan4php <aprodeosubarno@gmail.com>
2023-07-03 06:26:07 +0700

    belajar RUN instruction dan menambahkan komentar pada FROM instruction

--- belajar-docker-dockerfile/script.sh
+++ belajar-docker-dockerfile/script.sh
@@ -1,4 +1,14 @@
 # Untuk membuat image dari docker file, format perintahnya adalah:
 # docker build -t <nama_image>:<tag> <folder_yang_berisi_dockerfile>
+# jika ingin menampilkan detail dari proses build image, gunakan perintah --progress=plain
+# jika ingin mengulangi proses build tanpa cache, gunakan perintah --no-cache
 
-docker build -t kochan4php/from:1.0.0 from
+# FROM Instruction
+docker build -t kochan4php/from from
+
+docker image ls | grep kochan4php
+
+# FROM Instruction
+docker build -t kochan4php/run run --progress=plain --no-cache
+
+docker image ls | grep kochan4php

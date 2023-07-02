c448505f90af0e93a1ed017f585445d9ac43db1b
kochan4php <aprodeosubarno@gmail.com>
2023-07-02 15:00:12 +0700

    belajar Dockerfile instruksi FROM

---
+++ belajar-docker-dockerfile/script.sh
@@ -0,0 +1,4 @@
+# Untuk membuat image dari docker file, format perintahnya adalah:
+# docker build -t <nama_image>:<tag> <folder_yang_berisi_dockerfile>
+
+docker build -t kochan4php/from:1.0.0 from

106e71fec02f0c73883dbfe0f1391632ff3fcf6a
kochan4php <aprodeosubarno@gmail.com>
2023-07-03 22:31:38 +0700

    belajar melihat list container yang hanya dibuat menggunakan docker-compose.yaml

--- belajar-docker-compose/script.sh
+++ belajar-docker-compose/script.sh
@@ -5,3 +5,7 @@ docker compose create
 
 # Menjalankan container dari file docker-compose.yaml
 docker compose start
+
+# Melihat list container yang telah dibuat dari file docker-compose.yaml
+docker compose ps # Melihat semua container yang berjalan
+docker compose ps -a # Melihat semua container

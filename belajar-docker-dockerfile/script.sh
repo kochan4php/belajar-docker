39c6c324edf7e383fcf6746d09c98064e2da635e
kochan4php <aprodeosubarno@gmail.com>
2023-07-03 10:01:13 +0700

    belajar instruksi CMD dan LABEL

--- belajar-docker-dockerfile/script.sh
+++ belajar-docker-dockerfile/script.sh
@@ -12,3 +12,21 @@ docker image ls | grep kochan4php
 docker build -t kochan4php/run run --progress=plain --no-cache
 
 docker image ls | grep kochan4php
+
+# CMD Instruction
+docker build -t kochan4php/cmd cmd
+
+docker image ls | grep kochan4php
+
+docker container create --name cmd kochan4php/cmd
+
+docker container start cmd
+
+docker container logs cmd
+
+# LABEL Instruction
+docker build -t kochan4php/label label
+
+docker image ls | grep kochan4php
+
+docker image inspect kochan4php/label

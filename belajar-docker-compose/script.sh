5f878a1a12fc80cc4c10f9361b20306d13266942
kochan4php <aprodeosubarno@gmail.com>
2023-07-03 22:57:47 +0700

    belajar melihat project docker compose yang sedang berjalan

--- belajar-docker-compose/script.sh
+++ belajar-docker-compose/script.sh
@@ -15,3 +15,8 @@ docker compose stop
 
 # Menghapus semua container yang telah dibuat dari file docker-compose.yaml
 docker compose down # Bisa juga digunakan untuk menghapus container, network dan volume yang telah dibuat dari file docker-compose.yaml
+
+# Informasi konfigurasi Docker Compose akan disimpan di dalam project yang kita kerjakan.   
+# Secara default nama project-nya adalah nama folder dari file si docker-compose.yaml tersebut berada.
+# Untuk melihat daftar project yang sedang berjalan, gunakan perintah docker compose ls.
+docker compose ls
